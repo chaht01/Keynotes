95f18042dd7621ef4d4b77b3abfe173dba63b4d0
Hyuntak Cha <chaht01@gmail.com>
2019-01-20 23:46:08 +0900

    tested

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,7 @@
 /* global describe, it, before */
 
 import chai from "chai";
-import { Note, NoteObject, Longtake } from "../lib/Longtake.js";
+import { Note, NoteObject, Keynotes } from "../lib/Keynotes";
 
 chai.expect();
 
@@ -20,7 +20,8 @@ describe("Given an instance of my Note library", () => {
   });
   describe("when I need the Object", () => {
     it("should return the object", () => {
-      expect(lib.getObject()).to.be.equal([1, 2, 3]);
+      expect(lib.getObject()).to.be.an("array");
+      expect(lib.getObject()).to.deep.equal([1, 2, 3]);
     });
   });
 });
